746ed84b2c07fe18ee44445c2656629e47beecaa
patsaporn-phadungsil <68010852@kmitl.ac.th>
2025-10-02 08:26:44 +0700

    Update makiancode.c

--- makiancode.c
+++ makiancode.c
@@ -33,3 +33,47 @@ void  menu(){
 
     }
 
+void Search_menu()
+{
+    {
+        char line[200];
+        char search[100];
+        int found = 0, count = 0;
+        FILE *file = fopen("makiancode.csv", "r");
+        printf("Search data: ");
+        scanf("%s", search);
+
+        while(fgets(line, sizeof(line), file) != NULL)
+        {
+                if(strstr(line, search))
+            {
+                    if(count < 1)
+                {
+                    printf("+----------+------------+-------------+----------------+\n");
+                    printf("| Name     | Product Type   | Rating | Evaluation date  |\n");
+                    printf("+----------+------------+-------------+----------------+\n");
+                }
+            
+            char *ApplicantName = strtok(line, ",");
+            char *CareerType = strtok(NULL, ",");
+            char *Advice = strtok(NULL, ",");
+            char *AdviceDate = strtok(NULL, "\n");
+
+            printf("| %-8s | %-14s | %-6s | %-16s | %-3d |\n", ApplicantName, CareerType, Advice, AdviceDate);
+            found = 1;   
+            printf("+----------+------------+-------------+----------------+\n");
+            }
+            count++;
+        }
+       
+ if(found != 1) printf("Data is not found.\n");
+        fclose(file);
+    }
+
+}
+
+            fclose(file);
+
+    
+
+  
