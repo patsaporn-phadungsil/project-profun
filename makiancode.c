f2ec0ecca1c0583f63fcefec007bc2d87d976a8e
patsaporn-phadungsil <68010852@kmitl.ac.th>
2025-10-02 08:25:05 +0700

    Update makiancode.c

--- makiancode.c
+++ makiancode.c
@@ -5,4 +5,31 @@
 char ApplicantName[50];
 char CareerType[50];
 char Advice[100];
-float AdviceData;
+float AdviceDate;
+
+//menu ที่เป็นอาหาร แฮ่
+
+void  menu(){
+    
+     printf("AppicantName : ");
+        scanf("%s",ApplicantName);
+        printf("CareerType : ");
+        scanf("%s",CareerType);
+        printf("Advice : ");
+        scanf("%s",Advice);
+        printf("AdviceData (YYYY-MM-DD) : ");
+        scanf("%f",AdviceDate);   
+
+      
+
+        FILE*file=fopen("makiancode.csv","a");
+        if (file == NULL)
+        {
+                printf("can't open file");   
+                
+        }  
+        fprintf(file,"%s,%s,%s,%.2f\n" ,ApplicantName,CareerType,Advice,AdviceDate);   
+        fclose(file);
+
+    }
+
